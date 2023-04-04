498ea5757d7f293129199a14aa1de60064a2dfc4
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-04-04 12:26:09 -0500

    implemented further testing

--- StringListSearchTest.java
+++ StringListSearchTest.java
@@ -13,10 +13,31 @@ public class StringListSearchTest {
         int expectedOutput;
         int actualOutput;
 
-        // some positive numbers
+        // search hit
         inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
         inputTerm = "the";
         expectedOutput = 2;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search miss
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search last
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "mountain";
+        expectedOutput = 3;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search first
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "there";
+        expectedOutput = 0;
         actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
         assertEquals(expectedOutput, actualOutput);
     }
@@ -29,9 +50,32 @@ public class StringListSearchTest {
         int actualOutput;
 
         inputItems = new ArrayList<>(Arrays.asList("apple", "banana", "cherry", "duck"));
+        
+        //search hit
         inputTerm = "banana";
         expectedOutput = 1;
         actualOutput = StringListSearch.orderedLinearSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search miss
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search last
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "mountain";
+        expectedOutput = 3;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search first
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "there";
+        expectedOutput = 0;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
         assertEquals(expectedOutput, actualOutput);
     }
 
@@ -43,9 +87,32 @@ public class StringListSearchTest {
         int actualOutput;
 
         inputItems = new ArrayList<>(Arrays.asList("apple", "banana", "cherry", "duck"));
+        
+        //search hit
         inputTerm = "banana";
         expectedOutput = 1;
         actualOutput = StringListSearch.orderedBinarySearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search miss
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search last
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "mountain";
+        expectedOutput = 3;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        // search first
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "there";
+        expectedOutput = 0;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
         assertEquals(expectedOutput, actualOutput);
     }
 }
